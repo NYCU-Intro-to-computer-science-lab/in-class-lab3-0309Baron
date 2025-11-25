802c2e33a6b881df285e136278576cc6ea1251ce
0309Baron <binghaozhuang8@gmail.com>
2025-11-25 13:48:51 +0800

    Implement prime checking and pair finding logic
    Add function to check for prime numbers and use it to find pairs that sum to a given number.

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,25 @@
 #include <iostream>
 using namespace std;
 
+
+bool isPrime(int a){
+    for(int i=2;i<a;i++){
+        if(a%i==0)return false;
+        break;
+    }
+    return true;
+}
+
 int main() {
-    
+    int n;
+    cin >> n;
+    for(int i=2;i<=n/2;i++){
+        if(isPrime(i) && isPrime(n-i)){
+            cout << i << " " << n-i << endl;
+            break;
+        }
+    }
+
     
     return 0;
 }
