dc670483162127318ed3d32bf639f62e72f3b4ad
0309Baron <binghaozhuang8@gmail.com>
2025-11-25 14:17:36 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -4,8 +4,15 @@
 using namespace std;
 
 
-int main() {
-    
+void towerhanoi (int n, char A, char B, char C) {
+    if (n == 0) return;
+    towerhanoi(n-1, A, C, B);
+    cout << "Move ring " << n << " from " << A << " to " << C << endl;
+    towerhanoi(n-1, B, A, C);
+}
 
+int main(){
+    int a;
+    towerhanoi(a, '1', '2', '3');
     return 0;
 }
