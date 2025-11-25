315ca0695d8fad45875683f5ae6c7dbcc8fac7ed
0309Baron <binghaozhuang8@gmail.com>
2025-11-25 13:34:55 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,27 @@
 #include <iostream>
 using namespace std;
 
+long long factorial(int n){
+    if(n==0)return 0;
+    if(n==1)return 1;
+    if(n>1){
+        return n*factorial(n-1);
+    }
+}
+
+long long sum(int n){
+    if(n==0)return 0;
+    if(n==1)return 1;
+    if(n>1){
+        return n+sum(n-1);
+    }
+}
+
 int main() {
-    
+    int a;
+    cin >> a;
+    cout << factorial(a) << endl;
+    cout << sum(a) << endl;
     
     return 0;
 }
